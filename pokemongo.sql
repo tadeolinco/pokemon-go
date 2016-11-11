2ba417c9713cde13cb93aaa3bc0c6fe3d9a0a3d9
Carlo Serrano <cagserrano@gmail.com>
2016-11-11 23:38:09 +0800

    Added database schema

---
+++ pokemongo.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS pokemongo;
+CREATE DATABASE IF NOT EXISTS pokemongo;
+USE pokemongo;
+
+CREATE TABLE user (
+	user_id INT AUTO_INCREMENT,
+	name VARCHAR(25),
+    gender VARCHAR(10),
+    location VARCHAR(20),
+    date_registered DATE,
+    number_of_gyms_battled INT,
+    team VARCHAR(10),
+    level INT,
+    username VARCHAR(10),
+    password VARCHAR(10),
+    CONSTRAINT user_userid_pk PRIMARY KEY(user_id)
+);
+
+CREATE TABLE gym (
+    gym_id INT AUTO_INCREMENT,
+    name VARCHAR(25),
+    location VARCHAR(20),
+    number_of_users_battled INT,
+    team VARCHAR(10),
+    prestige VARCHAR(10),
+    CONSTRAINT user_gymid_pk PRIMARY KEY(gym_id)
+);
+
+CREATE TABLE pokemon (
+    pokemon_id INT AUTO_INCREMENT,
+    name VARCHAR(25),
+    cp INT,
+    type VARCHAR(10),
+    level INT,
+    date_caught DATE,
+    user_id INT,
+    gym_id INT,
+    CONSTRAINT user_pokemonid_pk PRIMARY KEY(pokemon_id),
+    CONSTRAINT user_userid_fk FOREIGN KEY(user_id) REFERENCES user(user_id),
+    CONSTRAINT user_gymid_fk FOREIGN KEY(gym_id) REFERENCES gym(gym_id)
+);
+
+CREATE TABLE challenges (
+    user_id INT,
+    gym_id INT,
+    CONSTRAINT user_userid_uk UNIQUE(user_id),
+    CONSTRAINT user_gymid_uk UNIQUE(gym_id)
+);
