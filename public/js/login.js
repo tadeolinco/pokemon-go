b8d440358c655edfcf71f01992c75b2925dc4a05
cagserrano <cagserrano22@gmail.com>
2016-11-27 20:14:00 +0800

    final login ui

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,19 @@
 $('select.dropdown')
   .dropdown()
 ;
+
+$('.ui.form')
+  .form({
+    fields: {
+      name: {
+        identifier: 'select sex',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Enter sex'
+          }
+        ]
+      }
+    }
+  })
+;
